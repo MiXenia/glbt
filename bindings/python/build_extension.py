4b2d72c703daebf8d335b46a935567466101620c
Kay Hood <kay.moth.fuzz@gmail.com>
2022-04-15 05:08:34 -0500

    vendor GLFW for python on windows

--- bindings/python/build_extension.py
+++ bindings/python/build_extension.py
@@ -4,9 +4,11 @@ ffibuilder = FFI()
 from sys import platform
 print(platform)
 if platform == "linux":
+    library_dirs = []
     libraries = ["glfw", "m", "dl"]
 elif platform == "win32":
-    libraries = ["glfw3"]
+    library_dirs = ["../../lib/vs2022"]
+    libraries = ["glfw3dll"]
 
 with open('../../include/glbt_interface.h', 'r') as file:
     ffibuilder.cdef(file.read())
@@ -24,6 +26,7 @@ ffibuilder.set_source("glbt",
         '../../src/texture.c',
         '../../src/window.c'
     ],
+    library_dirs=library_dirs,
     libraries=libraries)
 
 if __name__ == "__main__":
